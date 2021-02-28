774b98cb0ac98813de5d050f63d229d4af161f0c
rafaelle.xavier <rafaelle.xavier@insa.gov.br>
2021-02-28 14:03:03 -0300

    fix: correcting merge error

--- src/app/modules/site/login/login.module.ts
+++ src/app/modules/site/login/login.module.ts
@@ -1,10 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-=======
-import { FormsModule } from '@angular/forms';
->>>>>>> b99ddc66ae0ea1ba7986f5b8ea05595ef9af67eb
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
@@ -17,12 +13,8 @@ import { LoginComponent } from './login.component';
   imports: [
     CommonModule,
     LoginRoutingModule,
-<<<<<<< HEAD
     FormsModule,
     ReactiveFormsModule,
-=======
-    FormsModule
->>>>>>> b99ddc66ae0ea1ba7986f5b8ea05595ef9af67eb
   ],
   exports: [
     LoginComponent,

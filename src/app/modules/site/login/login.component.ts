89f6b0c325763bc142fba1b077e1122ebe981b5d
Pjmatesco <p.junior1992@gmail.com>
2021-02-28 00:12:26 -0300

    chore: validador login

--- src/app/modules/site/login/login.component.ts
+++ src/app/modules/site/login/login.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  @ViewChild('userInput') userInput: ElementRef;
+  @ViewChild('passwordInput') passwordInput: ElementRef;
 
-  ngOnInit(): void {
+  user: string;
+  password: string;
+
+  estaCarregando: boolean;
+  erroNoLogin: boolean;
+
+  constructor(
+    private router: Router
+  ) { }
+
+  onSubmit(form) {
+    form.controls.user.markAsTouched();
+    form.controls.password.markAsTouched();
+
+    if (form.controls.user.invalid) {
+      this.userInput.nativeElement.focus();
+      return;
+    }
+
+    if (form.controls.password.invalid) {
+      this.passwordInput.nativeElement.focus();
+      return;
+    }
+
+    return;
+  }
+
+  onSuccessLogin() {
+    this.router.navigate(['dashboard']);
+  }
+
+  onErrorLogin() {
+    this.erroNoLogin = true;
+  }
+
+  exibeErro(nomeControle: string, form: NgForm) {
+    if (!form.controls[nomeControle]) {
+      return false;
+    }
+
+    return form.controls[nomeControle].invalid && form.controls[nomeControle].touched;
   }
 
 }

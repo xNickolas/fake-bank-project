3f528f57a0b432f263666dc0471266aa2847622f
rafaelle <rafaelle.xavier@gmail.com>
2021-02-28 22:05:27 -0300

    chore: changing local sidebar

--- src/app/modules/logged-area/logged-area.module.ts
+++ src/app/modules/logged-area/logged-area.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SidebarComponent } from './dashboard/components/sidebar/sidebar.component';
 
 import { LoggedAreaRoutingModule } from './logged-area-routing.module';
 import { LoggedAreaComponent } from './logged-area.component';
@@ -9,6 +10,7 @@ import { LoggedAreaComponent } from './logged-area.component';
 @NgModule({
   declarations: [
     LoggedAreaComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,

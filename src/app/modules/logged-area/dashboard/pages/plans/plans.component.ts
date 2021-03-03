543b074eb7497b654282a437d1e390d60eeac9d4
xNickolas <nickolsctt@gmail.com>
2021-03-03 19:56:49 -0300

    feat: create new plans working

--- src/app/modules/logged-area/dashboard/pages/plans/plans.component.ts
+++ src/app/modules/logged-area/dashboard/pages/plans/plans.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { take } from 'rxjs/operators';
 import { Plans } from 'src/app/modules/logged-area/interfaces/plans.interface';
 
@@ -12,12 +14,21 @@ import { PlanService } from './plans.service';
 export class PlansComponent implements OnInit {
 
   plans: Plans[];
+  plansForm: FormGroup;
 
   constructor(
     private plansService: PlanService,
+    private fb: FormBuilder,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
+    this.plansForm = this.fb.group({
+      description: ['', Validators.required],
+      user: ['', Validators.required],
+      type: ['', Validators.required],
+    });
+
     this.loadPlans();
   }
 
@@ -33,6 +44,41 @@ export class PlansComponent implements OnInit {
 
   onSucessPlans(response: Plans[]) {
     this.plans = response;
+  }
+
+  createNewPlan() {
+    const plan = {
+      descricao: this.plansForm.value.description,
+      login: this.plansForm.value.user,
+      tipoMovimento: this.plansForm.value.type
+    };
+    this.plansService.createPlan(plan)
+    .pipe(
+      take(1),
+    )
+    .subscribe(
+      response => this.onSucessCreatePlan(),
+      error => console.log(error),
+    );
+  }
+
+  onSucessCreatePlan() {
+    this.router.navigate(['/#/dashboard/home']);
+  }
+
+  validatePlansForm() {
+    Object.keys(this.plansForm.controls).forEach(field => {
+      const control = this.plansForm.get(field);
+      control.markAllAsTouched();
+    });
+  }
+
+  onSubmit() {
+    if (this.plansForm.invalid) {
+      this.validatePlansForm();
+      return;
+    }
+    this.createNewPlan();
   }
 
 }
--- src/app/modules/logged-area/dashboard/pages/plans/plans.service.ts
+++ src/app/modules/logged-area/dashboard/pages/plans/plans.service.ts
@@ -4,6 +4,7 @@ import { Plans } from 'src/app/modules/logged-area/interfaces/plans.interface';
 import { environment } from 'src/environments/environment';
 
 import { AuthService } from './../../../../shared/services/auth/auth.service';
+import { CreatePlan } from './../../../interfaces/createPlan.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -19,15 +20,29 @@ export class PlanService {
 
 
   getPlans() {
-      const user = this.token.getUser();
-      const headers = new HttpHeaders({
-        Authorization: this.token.getToken()
-      });
-      const params = new HttpParams().set('login', user.login);
-      const httpOptions = {
-        headers,
-        params
-      };
-      return this.http.get<Plans[]>(`${this.API_URL}/lancamentos/planos-conta`, httpOptions);
+    const user = this.token.getUser();
+    const headers = new HttpHeaders({
+      Authorization: this.token.getToken()
+    });
+    const params = new HttpParams().set('login', user.login);
+    const httpOptions = {
+      headers,
+      params
+    };
+
+    return this.http.get<Plans[]>(`${this.API_URL}/lancamentos/planos-conta`, httpOptions);
+  }
+
+  createPlan(plan: CreatePlan) {
+    const user = this.token.getUser();
+    const headers = new HttpHeaders({
+      Authorization: this.token.getToken()
+    });
+    const params = new HttpParams().set('login', user.login);
+    const httpOptions = {
+      headers,
+      params
+    };
+    return this.http.post<CreatePlan>(`${this.API_URL}/lancamentos/planos-conta`, plan, httpOptions);
   }
 }
